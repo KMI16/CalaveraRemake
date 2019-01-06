340a515705596d8051f49d6b1653d863eeb8d5cf
KMI16 <kmi16@hft-leipzig.de>
2019-01-06 15:21:23 +0100

    added git-server and its corresponding setup and start scripts

---
+++ git-server/server-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# clear data from the git-folder if exists
+# create data/repos and data/keys if not exists
+
+[ ! -d ${PWD}/data ] && mkdir -p ${PWD}/data
+[ ! -d ${PWD}/data/repos ] && mkdir -p ${PWD}/data/repos
+[ ! -d ${PWD}/data/keys ] && mkdir -p ${PWD}/data/keys
+
+rm -f ${PWD}/data/repos/*
+rm -f ${PWD}/data/keys/*
+
+
+# create new ssh-key if not exists and copy it
+# into the keys folder of the git server
+if [ ! -f ~/.ssh/id_rsa.pub ]
+then
+    ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
+fi
+cp ~/.ssh/id_rsa.pub ${PWD}/data/keys
